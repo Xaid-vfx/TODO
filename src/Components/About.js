0c6dafc2cf578f4bf448635c1c26b5eb0ea65095
Xaid-vfx <77684943+Xaid-vfx@users.noreply.github.com>
2022-06-28 22:19:48 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div>About
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias necessitatibus nulla beatae, impedit tempore sunt minima magni cupiditate qui, hic velit reiciendis ab? A recusandae sunt, voluptatem nam officia asperiores?</p>
+    <div className='container py-5'>
+        <h4>What is a ToDo List?</h4>
+        <p>What is a ToDo List? The definition is a simple one. It’s a list of tasks you need to complete or things that you want to do.</p>
+        <p>Most typically, they’re organised in order of priority. Traditionally, they’re written on a piece of paper or post it notes and act as a memory aid. As technology has evolved we have been able to create a todo lists with excel spreadsheets, word documents, email lists, todo list apps, Microsoft to do and google to do list to name a few. You can use a to do list in your home and personal life, or in the workplace.</p>
+        <p>Having a list of everything you need to do written down in one place means you shouldn’t forget anything important. By prioritising the tasks in the list you plan the order in which you’re going to do them and can quickly see what needs your immediate attention and what tasks you can leave until a little later.</p>
+        <h4>The Benefits of Using a To Do List</h4>
+        <p>One of the most important reasons you should use a to do list is that it will help you stay organised. When you write all your tasks in a list, they seem more manageable. When you’ve got a clear outline of the tasks you’ve got to do and those you’ve completed, it helps you stay focused. While freeing up space in your mind for other more creative tasks.</p>
+        <p>When you complete a task, you can cross it off your list. This gives you a sense of progress and achievement, something you’ll lack if you’re always rushing from one task to the next. If you feel a sense of achievement, it spurs you on and motivates you to keep moving forward.</p>
+
     </div>
   )
 }
