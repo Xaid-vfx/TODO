397a20dbeed8801aeb791de1bc3b09704d7d3149
Xaid-vfx <77684943+Xaid-vfx@users.noreply.github.com>
2022-06-28 22:22:24 +0530

    Update header.js

--- src/Components/header.js
+++ src/Components/header.js
@@ -8,12 +8,12 @@ function BasicExample(props) {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">{props.title}</Navbar.Brand>
+        <Navbar.Brand href="home">{props.title}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="home">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
